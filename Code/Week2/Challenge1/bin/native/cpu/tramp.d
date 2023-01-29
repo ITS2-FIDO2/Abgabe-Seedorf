4c30aa4f9b8571d02bdc4628711ba7d737821eb6
LordOfReinkeness <post@lukas-reinke.de>
2023-01-29 11:51:30 +0100

    new gitignore

--- Code/Week2/Challenge1/bin/native/cpu/tramp.d
+++
@@ -1,6 +0,0 @@
-/home/lukas/Nextcloud/Development/HFT/Sem_6/ITS2/Riot/Abgabe-Seedorf/Code/Week2/Challenge1/bin/native/cpu/tramp.o: \
- /home/lukas/Nextcloud/Development/HFT/Sem_6/ITS2/Riot/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /home/lukas/Nextcloud/Development/HFT/Sem_6/ITS2/Riot/Abgabe-Seedorf/Code/Week2/Challenge1/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/lukas/Nextcloud/Development/HFT/Sem_6/ITS2/Riot/Abgabe-Seedorf/Code/Week2/Challenge1/bin/native/riotbuild/riotbuild.h:
